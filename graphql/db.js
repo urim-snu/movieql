d3c03e9b50bdc5df16b68b5cd0ad55f79a9587de
snu-urim <woorjb133@gmail.com>
2021-01-03 18:58:12 +0900

    Done

---
+++ graphql/db.js
@@ -0,0 +1,8 @@
+import fetch from "node-fetch";
+const API_URL = "https://yts.am/api/v2/list_movies.json";
+
+export const getMovies = (limit, rating) => {
+  return fetch(`${API_URL}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
+};
